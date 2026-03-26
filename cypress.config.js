2de7fc6fce8523ade53fa1e5592f787265dbfd57
Ruslana Yakymiv <ruslanayakymiv@gmail.com>
2026-03-26 20:07:32 +0200

    add cypress project id

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
+  projectId: "ojkz7p",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "https://telnyx.com",
   },
 });
